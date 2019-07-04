5193ce103d53908ca8175c44a711d4b65d041f7f
MahdiGam <52547909+MahdiGam@users.noreply.github.com>
2019-07-04 23:49:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const Guild = "";
+const voiceChannel = "";
 
+client.on("ready", () => {
+  console.log("Ready!");
+  client.channels.get(voiceChannel).join().then(connection => {
+    console.log("Successfully connection.");
+    setInterval(() => {
+      if(!client.guilds.get(Guild).voiceChannel || client.guilds.get(Guild).voiceChannel && client.guilds.get(Guild).voiceChannelID != voiceChannel) {
+        client.channels.get(voiceChannel).join();
+      }
+    }, 15000);
+  });
+});
+
+client.login(process.env.TOKEN);
